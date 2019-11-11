96b8db244efe1e1acb6c4aa56666c7bac6fde30b
Игорь Волчков <ihor.volchkov@gmail.com>
2019-11-11 11:11:50 +0200

    Create authorization filters

---
+++ src/main/java/project/filter/AdminSecurity.java
@@ -0,0 +1,37 @@
+package project.filter;
+
+import project.domain.user.User;
+import project.entity.user.Role;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter(dispatcherTypes = {
+        DispatcherType.REQUEST,
+        DispatcherType.FORWARD},
+        urlPatterns = {"/admin-service.jsp"})
+public class AdminSecurity implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+
+        if (user == null || user.getRole() != Role.ADMIN) {
+            request.getRequestDispatcher("error.jsp").forward(servletRequest,servletResponse);
+        }
+
+        filterChain.doFilter(servletRequest ,servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
---
+++ src/main/java/project/filter/ScrumMasterSecurity.java
@@ -0,0 +1,38 @@
+package project.filter;
+
+
+import project.domain.user.User;
+import project.entity.user.Role;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter(dispatcherTypes = {
+        DispatcherType.REQUEST,
+        DispatcherType.FORWARD},
+        urlPatterns = {"/scrum-master-service.jsp"})
+public class ScrumMasterSecurity implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+
+        if (user == null || user.getRole() != Role.SCRUM_MASTER) {
+            request.getRequestDispatcher("error.jsp").forward(servletRequest,servletResponse);
+        }
+
+        filterChain.doFilter(servletRequest ,servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
