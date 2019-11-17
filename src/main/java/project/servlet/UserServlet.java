5433819575b96d2c10fb7ac665b448b5062eccaa
Игорь Волчков <ihor.volchkov@gmail.com>
2019-11-17 13:43:59 +0200

    add annotations for servlets

--- src/main/java/project/servlet/UserServlet.java
+++ src/main/java/project/servlet/UserServlet.java
@@ -4,12 +4,14 @@ import project.command.Command;
 import project.context.ApplicationContextInjector;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Map;
 
+@WebServlet("/user")
 public class UserServlet extends HttpServlet {
     private final Map<String, Command> nameToCommand;
     private final Command defaultCommand;
