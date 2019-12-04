45aa920708c6286ee7634c3305ad81c8b0ffa7cf
Игорь Волчков <ihor.volchkov@gmail.com>
2019-12-04 23:58:41 +0200

    Add javadoc for command interface

--- src/main/java/com/ua/timetracking/command/Command.java
+++ src/main/java/com/ua/timetracking/command/Command.java
@@ -2,9 +2,30 @@ package com.ua.timetracking.command;
 
 import javax.servlet.http.HttpServletRequest;
 
+/**
+ * The root interface in the command hierarchy.
+ * Provides also two default methods for pagination and parsing parameter from {@link HttpServletRequest}
+ *
+ * @author Ihor Volchkov
+ **/
 public interface Command {
+    /**
+     * Returns String which represents what url will be shown.
+     *
+     * @param request element who has all needed information for processing command.
+     * @return String which represents what url will be shown
+     */
     String execute(HttpServletRequest request);
 
+    /**
+     * Returns String which represents what url will be shown.
+     *
+     * @param request        element who is needed to set appropriate attributes.
+     * @param command        element which has information for links with pagination.
+     * @param rows           element which set number of rows for pagination.
+     * @param currentPage    element which set current page for pagination.
+     * @param recordsPerPage element which set records per page for pagination.
+     */
     default void paginating(HttpServletRequest request, String command, int rows, int currentPage, int recordsPerPage) {
         int numberOfPages = rows / recordsPerPage;
 
@@ -18,6 +39,13 @@ public interface Command {
         request.setAttribute("recordsPerPage", recordsPerPage);
     }
 
+    /**
+     * Returns number after parsing from String parameter.
+     *
+     * @param request element who is needed to get appropriate attributes.
+     * @param param   element which will be parsed.
+     * @return the number which has already parsed.
+     */
     default int parseParameter(HttpServletRequest request, String param) {
         try {
             return Integer.parseInt(request.getParameter(param));
